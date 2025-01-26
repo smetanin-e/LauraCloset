4c41c7bf0c54ebf51cc598b285cb5ccd63921bd2
smetanin-e <e91smet15@gmail.com>
2025-01-26 19:46:31 +0300

    исправление багов

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*==================ASIDE========================*/
 
 let filterBlock = document.querySelector('.filter');
 let filter = document.querySelector('.filter__title');
@@ -11,7 +12,7 @@ filter.addEventListener("click", function (e) {
     console.log('Clicked on FILTER');
     aside.classList.toggle('mobile-aside-active');
     button.classList.toggle('_hide');
-    filterBlock.classList.toggle('_border');
+    filterBlock.classList.toggle('_hide');
     document.body.classList.toggle('_lock');
     wrapper.classList.toggle('_lock-wrapper');
 })
@@ -20,10 +21,24 @@ button.addEventListener("click", function (e) {
     console.log('Clicked on button');
     aside.classList.toggle('mobile-aside-active');
     button.classList.toggle('_hide');
-    filterBlock.classList.toggle('_border');
+    filterBlock.classList.toggle('_hide');
     document.body.classList.toggle('_lock');
     wrapper.classList.toggle('_lock-wrapper');
+});
+
+/*==================/ ASIDE========================*/
+
+/*=======================SORT=======================*/
+let sortButton = document.querySelector('.sort-items');
+let popupSorting = document.querySelector('.popup-sorting');
+
+sortButton.addEventListener("click", function (e) {
+    console.log('Clicked on SORT');
+    
+    sortButton.classList.toggle('сhoice-sorting');
+    popupSorting.classList.toggle('_hide');
 })
+/*=======================/ SORT=======================*/
 
  /*  if (aside.classList.contains("mobile-aside-active")) {
         console.log('true');
